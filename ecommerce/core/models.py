77241a9e65c3e841bc72232f64b4517f91e89283
magdalenakazior1 <134530196+magdalenakazior1@users.noreply.github.com>
2024-08-14 16:14:56 +0200

    first commit

---
+++ ecommerce/core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Customer(models.Model):
+
+    user = models.OneToOneField(User, null = False, blank = False, on_delete = models.CASCADE)
+
+    # extra fields will come here
+    phone_field = models.CharField(max_length=12, blank=False)
+
+
+    def __str__(self):
+        return self.user.username
